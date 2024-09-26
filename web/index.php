7d24efde394ed0fb9e27582f9a7bad12f7d9d508
Aivars <example@mail.com>
2024-09-26 10:00:43 +0300

    Created registration form

--- web/index.php
+++ web/index.php
@@ -6,6 +6,29 @@
     <title>Piesakies webināram!</title>
 </head>
 <body>
-    
+    <div id="form-container">
+        <form action="confirmation.php" method="post">
+            <label for="name">Ievadiet savu vārdu: </label>
+            <input type="text" name="name" id="name" required> <br> <br> 
+
+            <label for="e-mail">Ievadiet savu e-pastu: </label>
+            <input type="text" name="e-mail" id="e-mail" required> <br> <br> 
+
+            <select name="difficulty" id="difficulty" required>
+                <option value="">--Izvēlaties kursa līmeni--</option>
+                <option value="Beginner">Beginner</option>
+                <option value="Intermediate">Intermediate</option>
+                <option value="Advanced">Advanced</option>
+            </select> <br> <br>
+
+            <label for="info">Vai vēlies saņemt info par citiem mūsu organizētajiem pasākumiem?</label> <br>
+
+            <label for="yes">Yes:</label>
+            <input type="radio" name="info" id="yes">
+
+            <label for="no">No:</label>
+            <input type="radio" name="info" id="no">
+        </form>
+    </div>
 </body>
 </html>

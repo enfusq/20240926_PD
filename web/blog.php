4610f50eced4ee665d21e2891bc93e59ce8583c0
Aivars <example@mail.com>
2024-09-26 10:05:45 +0300

    Added blog elements

--- web/blog.php
+++ web/blog.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Blog</title>
 </head>
 <body>
-    
+    <h1>Tiešsaites dienasgrāmata</h1>
+
+    <div id="biography">
+        <h2>Biogrāfija</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint eligendi explicabo magni eum possimus! Nobis amet dolorum, earum corporis ab quae inventore. Consequuntur odio molestiae perferendis, sit repellendus nisi tempora? Veniam veritatis, nisi nemo libero deleniti velit optio fugit sint blanditiis a, culpa distinctio. Asperiores quos suscipit, nulla a voluptatum, ex nihil consectetur perferendis officia consequatur placeat inventore voluptate omnis facere minima itaque? Libero enim fugiat assumenda sed accusantium dolores eius laudantium dolorem consequuntur a quod et laboriosam eum alias odio velit sapiente fugit corrupti quam esse voluptatibus, cupiditate expedita quasi? Adipisci, vero similique excepturi sequi sint quas quae illum.</p>
+
+    </div>
+
+    <div id="why-programmer">
+        <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+
+        <ul>
+            <li>Laba alga</li>
+            <li>Vienmēr ir patikuši datori</li>
+            <li>Kādreiz bija ideja veidot spēles</li>
+            <li>Lai labāk saprastu datorus</li>
+            <li>Ļoti interesanti ir ko tu vari paveikt ar programmēšanu</li>
+        </ul>
+    </div>
 </body>
 </html>
